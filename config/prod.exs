f1232e8ae7cb4fed75e003c8c38beed43498a004
mnecibi <mehdi.necibi@capgemini.com>
2022-06-17 19:07:36 +0200

    fixed typo

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :scores, ScoresWeb.Endpoint,
-  url: [host: "scores.necibi.com", port:443, scheme: "https"],
+  url: [host: "scores.necibi.com", port: 4001, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manigest.json"
 
 # Do not print debug messages in production
